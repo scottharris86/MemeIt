258d4f776e3ccb6271eea4730456e788a8492a2e
scottharris86 <scottharrisdev@gmail.com>
2020-02-01 20:23:27 -0500

    Initial Commit

---
+++ MemeIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeIt
+//
+//  Created by scott harris on 2/1/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
