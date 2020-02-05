c5b4a7e1a08dc3c32f65139366b3b79f5b100438
scottharris86 <scottharrisdev@gmail.com>
2020-02-05 12:51:14 -0500

    clean up files and folders and ut everything where it belongs

--- MemeIt/Models/Meme.swift
+++ MemeIt/Models/Meme.swift
@@ -1,22 +1,61 @@
 //
-//  Meme.swift
+//  MemeModel.swift
 //  MemeIt
 //
-//  Created by scott harris on 2/2/20.
+//  Created by Chris Gonzales on 2/2/20.
 //  Copyright © 2020 scott harris. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
+enum MemeCategory: String, CaseIterable{
+    case uncategorized = "Not Categorized"
+    case food = "Food Reference"
+    case movie = "Movie Refernence"
+    case sports = "Sport Reference"
+    case work = "Work Reference"
+    case personal = "Personal Reference"
+}
 
-struct Meme {
-    var name: String
-    var imageData: Data
+class Meme: Codable{
     
+    var category: MemeCategory
+    var imageData: Data
+    var isFavorite: Bool
     var image: UIImage? {
-        let theImage = UIImage(data: imageData)
-        return theImage
+        return UIImage(data: imageData)
     }
     
+    init(category: MemeCategory, imageData: Data, isFavorite: Bool = false){
+        self.category = category
+        self.imageData = imageData
+        self.isFavorite = isFavorite
+    }
+}
+
+extension MemeCategory: Codable {
+    enum Key: CodingKey {
+        case rawValue
+    }
+    enum CodingError: Error {
+        case unknownValue
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        let rawValue = try container.decode(String.self, forKey: .rawValue)
+        self = MemeCategory(rawValue: rawValue) ?? .uncategorized
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Key.self)
+        try container.encode(rawValue, forKey: .rawValue)
+    }
+}
+
+// MARK - Extensions
+
+extension Meme: Equatable{
+    static func == (lhs: Meme, rhs: Meme) -> Bool {
+        if lhs.imageData == rhs.imageData{ return true} else {return false}
+    }
 }
