fe327ab71817b4878ebbb5d324081fec605e8f9d
scottharris86 <scottharrisdev@gmail.com>
2020-02-05 22:00:39 -0500

    migrated search controller everywhere to library so i can now actually create the real search controller.

---
+++ MemeIt/Views/Cells/LibraryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryCollectionViewCell.swift
+//  MemeIt
+//
+//  Created by Chris Gonzales on 2/2/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+
+class LibraryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var LibraryImageView: UIImageView!
+}
